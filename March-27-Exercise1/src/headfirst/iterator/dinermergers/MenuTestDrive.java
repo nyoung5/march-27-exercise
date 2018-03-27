ddf8fb5b34a864204add6bd3d874d14e6c82b810
Nathan Young <nyoung5@duke303-25.elon.edu>
2018-03-27 12:11:41 -0400

    finished ex1, need to copy to ex2,

--- March-27-Exercise1/src/headfirst/iterator/dinermergers/MenuTestDrive.java
+++ March-27-Exercise1/src/headfirst/iterator/dinermergers/MenuTestDrive.java
@@ -5,8 +5,9 @@ public class MenuTestDrive {
   public static void main(String args[]) {
     PancakeHouseMenu pancakeHouseMenu = new PancakeHouseMenu();
     DinerMenu dinerMenu = new DinerMenu();
+    VesuvioMenu vesuvioMenu = new VesuvioMenu();
 
-    Waitress waitress = new Waitress(pancakeHouseMenu, dinerMenu);
+    Waitress waitress = new Waitress(pancakeHouseMenu, dinerMenu, vesuvioMenu);
     waitress.printMenu();
   }
 
--- March-27-Exercise1/src/headfirst/iterator/dinermergers/Waitress.java
+++ March-27-Exercise1/src/headfirst/iterator/dinermergers/Waitress.java
@@ -1,20 +1,24 @@
 package headfirst.iterator.dinermergers;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Waitress {
 
   private PancakeHouseMenu pancakeHouseMenu;
   private DinerMenu dinerMenu;
+  private VesuvioMenu vesuvioMenu;
 
-  public Waitress(PancakeHouseMenu pancakeHouseMenu, DinerMenu dinerMenu) {
+  public Waitress(PancakeHouseMenu pancakeHouseMenu, DinerMenu dinerMenu, VesuvioMenu vesuvioMenu) {
     this.pancakeHouseMenu = pancakeHouseMenu;
     this.dinerMenu = dinerMenu;
+    this.vesuvioMenu = vesuvioMenu;
   }
 
   public void printMenu() {
     ArrayList<MenuItem> menuItems = pancakeHouseMenu.getMenuItems();
     MenuItem[] menuItemsAs = dinerMenu.getMenuItems();
+    LinkedList<MenuItem> menuItemsLL = vesuvioMenu.getMenuItems();
 
     System.out.println("MENU\n----\nBREAKFAST");
     for (int i = 0; i < menuItems.size(); i++) {
@@ -25,7 +29,15 @@ public class Waitress {
     }
     System.out.println("\nLUNCH");
     for (int i = 0; i < menuItemsAs.length; i++) {
+    	if(menuItemsAs[i]!=null){
       MenuItem menuItem = menuItemsAs[i];
+      System.out.print(menuItem.getName() + ", ");
+      System.out.print(menuItem.getPrice() + " -- ");
+      System.out.println(menuItem.getDescription());
+    	}
+    }
+    System.out.println("\nDINNER");
+    for(MenuItem menuItem : menuItemsLL) {
       System.out.print(menuItem.getName() + ", ");
       System.out.print(menuItem.getPrice() + " -- ");
       System.out.println(menuItem.getDescription());
